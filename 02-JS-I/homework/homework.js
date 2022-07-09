9252218150bb8f3c264eb9d56ce1b1557e04794c
SIGMARIPPER <matiasgamboa2011@hotmail.com>
2022-07-09 20:27:43 -0300

    30 complete

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -161,33 +161,34 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var exclama = (str + "!"); return exclama
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var NyA = (nombre + " " + apellido); return NyA;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  var saludo = ("Hola " + nombre + "!"); return saludo;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  var area = (ancho * alto); return area
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  var peri = (lado * 4); return peri 
 }
 
 
